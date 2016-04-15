ed00a59c2f1cadb0a06d429acefd15c669a44121
Ben Greene <ben@windshieldinc.com>
2016-04-15 14:00:46 -0400

    upgrade to 0.8.0

--- package.js
+++ package.js
@@ -1,16 +1,17 @@
 Package.describe({
     name: 'benmgreene:bootstrap-tagsinput',
     summary: 'jQuery plugin providing a Twitter Bootstrap user interface for managing tags, repackaged for Meteor',
-    version: '0.6.0',
+    version: '0.8.0',
     git: "https://github.com/benmgreene/meteor-bootstrap-tagsinput.git"
 });
 
 Package.on_use(function (api) {
     // dependecies required by package
     api.use('jquery@1.0.1', 'client');
+    api.use('less', 'client');
     //api.use('mizzao:bootstrap-3@3.3.1','client')
     // adding the required file for package
     api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.min.js', 'client');
-    api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.css', 'client');
+    api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput.less', 'client');
    // api.add_files('lib/bootstrap-tagsinput/dist/bootstrap-tagsinput-angular.js', 'client');
 });
